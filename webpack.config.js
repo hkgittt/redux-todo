b92c49432119b8b0735cea9bc2c59697b621895a
Hyunkyu Yoon <hkyoon85@gmail.com>
2016-04-14 17:37:16 +0900

    Add file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
       { test: /\.jsx$/, loader: 'babel-loader' },
       { test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader' },
       { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.(png|jpg|jpeg|gif|woff|svg)$/, loader: 'file-loader' },
     ],
   },
   resolve: {
