eb06e593ff83087449ea6cec961912f928d15803
Hyunkyu Yoon <hkyoon85@gmail.com>
2016-06-20 19:11:48 +0900

    Remove unused code

--- source/actions.js
+++ source/actions.js
@@ -4,13 +4,6 @@ import * as api from './api';
 
 export const ADD_TODO = 'ADD_TODO';
 export const TOGGLE_TODO = 'TOGGLE_TODO';
-export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
-
-export const VisibilityFilters = {
-  SHOW_ALL: 'SHOW_ALL',
-  SHOW_COMPLETED: 'SHOW_COMPLETED',
-  SHOW_ACTIVE: 'SHOW_ACTIVE',
-};
 
 const receiveTodos = (response, filter) => ({
   type: 'RECEIVE_TODOS',
